1c0bcf15bdd944c8f27e6e23fcd74d001a855433
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-10-04 21:43:52 +0100

    35 - created addToConfigurationIfDoesNotExist

--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,11 @@
 module Main where
 
 import Control.Monad (unless)
-import Data.List (isInfixOf)
 import Data.Maybe (isNothing, Maybe(..))
-import Data.Text (pack, replace)
-import Data.Text.IO (putStrLn)
-import Prelude(IO, ($))
+import Data.Text (Text, replace, isInfixOf)
+import Data.Text.IO (putStrLn, readFile, writeFile)
+import Prelude(IO, ($), (<>))
 import System.Directory (findExecutable)
-import System.IO (readFile, writeFile)
 
 main :: IO ()
 main = do
@@ -21,12 +19,13 @@ main = do
     putStrLn "Installing Haskell GHC and cabal-install"
     configurationNix <- readFile "/etc/nixos/configuration.nix"
     let isGhcInstalled = "haskell.compiler.ghc865" `isInfixOf` configurationNix
-    let configurationNixWithGhc = if isGhcInstalled then pack configurationNix else
+    let configurationNixWithGhc = if isGhcInstalled then configurationNix else
          replace
            "environment.systemPackages = with pkgs; ["
            "environment.systemPackages = with pkgs; [\n\
            \    haskell.compiler.ghc865"
-           (pack configurationNix)
+           configurationNix
+
     -- let newConfigurationNix =
     --      replace
     --        "environment.systemPackages = with pkgs; ["
@@ -36,6 +35,21 @@ main = do
     --        \    atom"
     --        (pack configurationNix)
     putStrLn configurationNixWithGhc
+    writeFile "/etc/nixos/configuration.nix" configurationNixWithGhc
+
+environmentSystemPackages :: Text
+environmentSystemPackages = "environment.systemPackages = with pkgs; ["
+
+addToConfigurationIfDoesNotExist :: Text -> Text -> Text
+addToConfigurationIfDoesNotExist configurationNix package =
+  if isPackageInstalled then configurationNix else
+       replace
+         environmentSystemPackages
+         (environmentSystemPackages <> "\n\
+         \    " <> package)
+         configurationNix
+  where
+    isPackageInstalled = package `isInfixOf` configurationNix
 
 -- TODO install GHC, cabal-install, Atom if not already installed
 
