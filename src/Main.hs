9485fcdfea572b698c503b751bb1f18ff769d753
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-10-04 13:55:23 +0100

    Added TOODs

--- src/Main.hs
+++ src/Main.hs
@@ -2,3 +2,11 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+-- TODO install Nix if not already installed
+
+-- TODO install GHC and cabal-install if not already installed
+
+-- TODO install the chosen editor if not already installed
+
+-- TODO install the editor extensions if not already installed
