f514608e632e4e5b55ffa570361631475d39c865
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-10-04 21:14:39 +0100

    35 - install GHC

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Monad (unless)
-import Data.Maybe (isNothing)
+import Data.List (isInfixOf)
+import Data.Maybe (isNothing, Maybe(..))
+import Data.Text (pack, replace)
+import Data.Text.IO (putStrLn)
+import Prelude(IO, ($))
 import System.Directory (findExecutable)
+import System.IO (readFile, writeFile)
 
 main :: IO ()
 main = do
@@ -12,10 +19,25 @@ main = do
     _      -> "NixOS operating system not found"
   unless (isNothing maybeFilePath) $ do
     putStrLn "Installing Haskell GHC and cabal-install"
+    configurationNix <- readFile "/etc/nixos/configuration.nix"
+    let isGhcInstalled = "haskell.compiler.ghc865" `isInfixOf` configurationNix
+    let configurationNixWithGhc = if isGhcInstalled then pack configurationNix else
+         replace
+           "environment.systemPackages = with pkgs; ["
+           "environment.systemPackages = with pkgs; [\n\
+           \    haskell.compiler.ghc865"
+           (pack configurationNix)
+    -- let newConfigurationNix =
+    --      replace
+    --        "environment.systemPackages = with pkgs; ["
+    --        "environment.systemPackages = with pkgs; [\n\
+    --        \    haskell.compiler.ghc865\n\
+    --        \    haskellPackages.cabal-install\n\
+    --        \    atom"
+    --        (pack configurationNix)
+    putStrLn configurationNixWithGhc
 
--- TODO install GHC and cabal-install if not already installed
-
--- TODO install Atom if not already installed
+-- TODO install GHC, cabal-install, Atom if not already installed
 
 -- TODO install HIE if not already installed
 
