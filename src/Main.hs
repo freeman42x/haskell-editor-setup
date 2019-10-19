44d3b28199108b3c819e81a0de51ecf0b3414231
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-10-19 19:53:13 +0100

    44 - Added bulma CSS

--- src/Main.hs
+++ src/Main.hs
@@ -16,17 +16,15 @@ type Model = Int
 
 -- | Sum type for application events
 data Action
-  = AddOne
-  | SubtractOne
-  | NoOp
-  | SayHelloWorld
+  = NoOp
+  | Install
   deriving (Show, Eq)
 
 -- | Entry point for a miso application
 main :: IO ()
 main = JSaddle.run 8080 $ startApp App {..}
   where
-    initialAction = SayHelloWorld -- initial action to be executed on application load
+    initialAction = NoOp -- initial action to be executed on application load
     model  = 0                    -- initial model
     update = updateModel          -- update function
     view   = viewModel            -- view function
@@ -36,23 +34,21 @@ main = JSaddle.run 8080 $ startApp App {..}
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
-updateModel AddOne m = noEff (m + 1)
-updateModel SubtractOne m = noEff (m - 1)
 updateModel NoOp m = noEff m
-updateModel SayHelloWorld m = m <# do
+updateModel Install m = m <# do
   liftIO (putStrLn "Hello World") >> pure NoOp
 
 -- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
-viewModel x = div_ [] [
-   label_ [] [ text "Editor / IDE" ]
+viewModel x = form_ [] [
+   link_ [ rel_ "stylesheet", href_ "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" ]
+ , label_ [] [ text "Editor / IDE" ]
  , br_ []
  , label_ [] [ text "Atom" ]
  , input_ [ type_ "radio", name_ "editor", value_ "Atom" ]
  , label_ [] [ text "Visual Studio Code" ]
  , input_ [ type_ "radio", name_ "editor", value_ "VSCode", disabled_ True ]
  , br_ []
- , button_ [ onClick AddOne ] [ text "+" ]
  , text (ms x)
- , button_ [ onClick SubtractOne ] [ text "-" ]
+ , button_ [ onClick Install ] [ text "Install" ]
  ]
