f29e3d39e2b267a825e56080ee62d35e1931fd08
bredor <danil123550@gmail.com>
2020-04-12 02:28:20 +0300

    43 - TODO Debian/Atom automation

---
+++ src/OS/Linux/Debian.hs
@@ -0,0 +1,8 @@
+module OS.Linux.Debian where
+
+import           Prelude                        ( IO )
+import           Data.Text.IO                   ( putStrLn )
+
+
+debianAtom :: IO ()
+debianAtom = putStrLn "not yet!"
