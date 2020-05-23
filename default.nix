75f2fd5aa13ee3f315f73ee26982ab2a5e4f86af
Jor Bashllari <altnate2000@gmail.com>
2020-05-23 23:37:13 +0200

    add `nwjs-sdk` to HES buildInputs

--- default.nix
+++ default.nix
@@ -1,6 +1,15 @@
-with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
-  sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
-}) {});
-with pkgs.haskell.packages;
-ghc865.callCabal2nix "haskell-editor-setup" ./. { miso = ghc865.miso-jsaddle; }
+let
+  ghcVersion = "ghc865";
+  misoPkgs = import (builtins.fetchTarball {
+    url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
+    sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
+  }) {};
+in
+
+with misoPkgs.pkgs;
+with haskell.packages."${ghcVersion}";
+let app = callCabal2nix "haskell-editor-setup" ./. { miso = miso-jsaddle; };
+in  app.overrideAttrs (old: {
+  miso = miso-jsaddle;
+  buildInputs = old.buildInputs ++ [ nwjs-sdk ];
+})
