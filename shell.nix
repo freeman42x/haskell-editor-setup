0016380cd07d566d535df6ffdab3613c4704f2e2
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-05-26 13:02:35 +0100

    102 - `nix-shell` with `hie`, `ghcid`

--- shell.nix
+++ shell.nix
@@ -1,1 +1,9 @@
-let pkgs = import <nixpkgs> {}; in (import ./.).env.overrideAttrs (_: { shellHook = ''PATH="$PATH:${pkgs.ghc}/bin:${pkgs.cabal-install}/bin"''; })
+let pkgs = import <nixpkgs> { };
+in (import ./.).env.overrideAttrs (_: {
+  shellHook = ''
+    PATH="$PATH:${pkgs.ghc}/bin:${pkgs.cabal-install}/bin:${pkgs.ghcid}/bin:${
+      ((import
+        (fetchTarball "https://github.com/infinisil/all-hies/tarball/master")
+        { }).selection { selector = p: { inherit (p) ghc865; }; })
+    }/bin"'';
+})
