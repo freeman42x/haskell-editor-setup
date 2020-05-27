2303fe491f8db5275585fa736b29eb9b2f26dbcf
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-05-27 04:25:37 +0100

    119 - NWJS is now provided by nix-shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let pkgs = import <nixpkgs> { };
 in (import ./.).env.overrideAttrs (_: {
   shellHook = ''
-    PATH="$PATH:${pkgs.ghc}/bin:${pkgs.cabal-install}/bin:${pkgs.ghcid}/bin:${
+    PATH="$PATH:${pkgs.ghc}/bin:${pkgs.cabal-install}/bin:${pkgs.nwjs-sdk}/bin:${pkgs.ghcid}/bin:${
       ((import
         (fetchTarball "https://github.com/infinisil/all-hies/tarball/master")
         { }).selection { selector = p: { inherit (p) ghc865; }; })
