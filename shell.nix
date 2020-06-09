7f95229fce41feedd19989e88f6b78ddf1c5bfaa
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-06-09 22:22:12 +0100

    129 - add vscodeEnv to shell.nix

--- shell.nix
+++ shell.nix
@@ -5,6 +5,12 @@
     rev = "f75d62941d8ad616bfc334ae19d71a0a6677dd19";
     sha256 = "0n88r4fw1syad9zl7r40r7xlxwx38ni8s9hzyayxssr21ii2p38h";
 }) {}
+, nixpkgsCabal3 ?  import (ps.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "5272327b81ed355bbed5659b8d303cf2979b6953";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+}) {}
 , ghcVersion ? "ghc865"
 }:
 with nixpkgs;
@@ -16,7 +22,11 @@ let
 in
 drv.env.overrideAttrs (shellEnv: {
   buildInputs = shellEnv.buildInputs ++ [
+    nwjs-sdk
+    atom
+    git
     cabal2nix
+    nixpkgsCabal3.haskell.packages.${ghcVersion}.cabal-install
     hie
     (vscode-utils.vscodeEnv {
       #usually this path go to tmp but feel free to change it wherever you think it is good place to the "fake" global folder
@@ -37,6 +47,5 @@ drv.env.overrideAttrs (shellEnv: {
   ];
 
   shellHook = ''
-    echo 'to start editor with HIE run on the root of the folder: code .'
   '';
 })
