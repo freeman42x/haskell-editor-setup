6788b029c65b8a8a6e7e1ccc912e9ddd5c929ca8
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-06-13 05:46:47 +0100

    55 - Refactor to use same Atom setup implementation on all Linux distributions

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,7 @@
--- | Haskell language pragma
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE RecordWildCards #-}
 
--- | Haskell module declaration
 module Main where
 
--- | Miso framework import
 import Control.Lens ((.~), (&), makeLenses)
 import Control.Monad.IO.Class (liftIO)
 import Control.Concurrent (forkIO)
@@ -108,84 +103,3 @@ viewModel m = form_ [] [
  , br_ []
  , button_ [ clickHandler Install , class_ "button" ] [ text "Install" ]
  ]
-
-
-
-nixOsAtom :: IO ()
-nixOsAtom = do
-  maybeFilePath <- findExecutable "nixos-version"
-  putStrLnGreen $ case maybeFilePath of
-    Just _ -> "NixOS operating system detected"
-    _      -> "NixOS operating system not found"
-  unless (isNothing maybeFilePath) $ do
-    putStrLnGreen "Adding Haskell GHC and cabal-install to configuration.nix"
-    config <- readFile configurationNix
-    let newConfig = foldl' addToConfigurationIfDoesNotExist config ["haskell.compiler.ghc865", "haskellPackages.cabal-install", "atom"]
-    writeFile configurationNix newConfig
-    putStrLnGreen "Finished adding Haskell GHC and cabal-install to configuration.nix"
-
-    putStrLnGreen "Installing GHC, cabal-install and Atom"
-    exitCode <- shell "nixos-rebuild switch" empty
-    case exitCode of
-        ExitSuccess   -> return ()
-        ExitFailure n -> die ("nixos-rebuild switch failed with exit code: " <> repr n)
-    putStrLnGreen "Finished installing GHC, cabal-install and Atom"
-
-    putStrLnGreen "Adding Haskell IDE Engine to configuration.nix"
-    config2 <- readFile configurationNix
-    let newConfig2 =
-          addToConfigurationIfDoesNotExist
-            config2
-            "((import (fetchTarball \"https://github.com/infinisil/all-hies/tarball/master\")\
-            \ {}).selection { selector = p: { inherit (p) ghc865 ghc864; }; })"
-    writeFile configurationNix newConfig2
-    putStrLnGreen "Finished adding Haskell IDE Engine to configuration.nix"
-
-    putStrLnGreen "Installing Haskell IDE Engine"
-    exitCode2 <- shell "nixos-rebuild switch" empty
-    case exitCode2 of
-        ExitSuccess   -> return ()
-        ExitFailure n -> die ("nixos-rebuild switch failed with exit code: " <> repr n)
-    putStrLnGreen "Finished installing Haskell IDE Engine"
-
-    installAtomPackage "nix"
-    installAtomPackage "atom-ide-ui"
-    installAtomPackage "autocomplete-haskell"
-    installAtomPackage "hasklig"
-    installAtomPackage "ide-haskell-cabal"
-    installAtomPackage "ide-haskell-hasktags"
-    installAtomPackage "ide-haskell-hie"
-    installAtomPackage "ide-haskell-hoogle"
-    installAtomPackage "ide-haskell-repl"
-    installAtomPackage "language-haskell"
-
-
-
-putStrLnGreen :: Text -> IO ()
-putStrLnGreen str = putStrLn $ "\x1b[32m" <> str <> "\x1b[0m"
-
-configurationNix :: String
-configurationNix = "/etc/nixos/configuration.nix"
-
-environmentSystemPackages :: Text
-environmentSystemPackages = "environment.systemPackages = with pkgs; ["
-
-addToConfigurationIfDoesNotExist :: Text -> Text -> Text
-addToConfigurationIfDoesNotExist configNix package =
-  if isPackageInstalled then configNix else
-       replace
-         environmentSystemPackages
-         (environmentSystemPackages <> "\n\
-         \    " <> package)
-         configNix
-  where
-    isPackageInstalled = package `isInfixOf` configNix
-
-installAtomPackage :: Text -> IO ()
-installAtomPackage package = do
-  putStrLnGreen $ "Installing " <> package
-  exitCode <- shell ("sudo -u $SUDO_USER apm install " <> package) empty
-  case exitCode of
-      ExitSuccess   -> return ()
-      ExitFailure n -> die ("apm install failed with exit code: " <> repr n)
-  putStrLnGreen $ "Finished installing " <> package
--- src/OS/Linux.hs
+++ src/OS/Linux.hs
@@ -1,3 +1,101 @@
 module OS.Linux where
 
--- TODO
+import           Control.Monad.IO.Class         ( liftIO )
+import           Prelude                        ( IO
+                                                , (<$>)
+                                                , ($)
+                                                , (<>)
+                                                , (==)
+                                                , mapM_
+                                                , uncurry
+                                                )
+import           Data.Bifoldable                ( bifold )
+import           Data.Text                      ( replace
+                                                , isInfixOf
+                                                , Text)
+import           Data.Text.IO                   ( readFile
+                                                , writeFile
+                                                )
+import           Miso.Effect
+import           Miso.String                    ( toMisoString
+                                                , MisoString)
+import           Turtle                         ( sh
+                                                , inshellWithErr
+                                                , empty
+                                                )
+import           Turtle.Line                    ( lineToText )
+
+import           Types
+
+data ExtensionInfo = ExtensionInfo MisoString Text
+
+nixOsAtom :: Sink Action -> IO ()
+nixOsAtom sink = do
+  -- TODO move to utils
+  let log text = sink $ Append (text <> "\n")
+      -- TODO move to utils
+      logStep text actions = do
+        log $ "BEGIN : " <> text
+        _ <- actions
+        log $ "END   : " <> text
+      -- TODO move to utils
+      configurationNixFile = "/etc/nixos/configuration.nix"
+      -- TODO move to utils
+      environmentSystemPackages = "environment.systemPackages = with pkgs; ["
+      -- TODO move to utils
+      runShellCommand command = sh $ do
+        out <- inshellWithErr command empty
+        liftIO $ log $ toMisoString $ lineToText $ bifold out
+      -- TODO move to utils
+      configureAndInstall (ExtensionInfo name package) =
+        logStep ("Configuring " <> name) $ do
+          oldConfigurationNixText <- liftIO $ readFile configurationNixFile
+
+          -- FIXME vvv requires Nix parsing using HNIX
+          let newConfigurationNixText =
+                if isPackagePresent
+                  then oldConfigurationNixText
+
+                  else replace
+                    environmentSystemPackages
+                    (environmentSystemPackages <> "\n\
+                         \    " <> package)
+                    oldConfigurationNixText
+                where isPackagePresent = package `isInfixOf` oldConfigurationNixText
+          -- ^^^
+
+          liftIO $ writeFile configurationNixFile newConfigurationNixText
+          if oldConfigurationNixText == newConfigurationNixText -- OPTIMIZE
+            then log "Nix package already installed"
+            else logStep (toMisoString package) (runShellCommand "nixos-rebuild switch")
+
+      -- TODO move to utils
+      -- TODO install or update? extension or log message
+      -- TODO ensure extensions are enabled if not enable them
+      configureAtomPackage package = do
+        -- wrap in logStep
+        --   check if package isAtomPackageInstalled
+        --   install if not installed
+        --   update if installed
+
+        let installingPackage = "Installing Atom package - " <> toMisoString package
+        logStep installingPackage $
+          runShellCommand ("sudo -u $SUDO_USER apm install --color false " <> package)
+
+  mapM_ configureAndInstall $ uncurry ExtensionInfo <$>
+    [ ("Haskell GHC", "haskell.compiler.ghc865")
+    , ("cabal-install", "haskellPackages.cabal-install")
+    , ("Atom", "atom")
+    , ("Haskell IDE Engine", "((import (fetchTarball \"https://github.com/infinisil/all-hies/tarball/master\")\
+    \ {}).selection { selector = p: { inherit (p) ghc865; }; })") ]
+
+  mapM_ configureAtomPackage [ "nix"
+                              , "atom-ide-ui"
+                              , "autocomplete-haskell"
+                              , "hasklig"
+                              , "ide-haskell-cabal"
+                              , "ide-haskell-hasktags"
+                              , "ide-haskell-hie"
+                              , "ide-haskell-hoogle"
+                              , "ide-haskell-repl"
+                              , "language-haskell" ]
