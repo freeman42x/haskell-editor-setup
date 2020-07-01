828ea541a61c86e18e29cb5ae01c25572e4d1f7f
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-07-01 20:40:46 +0100

    55 - Refactor to use same Atom setup implementation on all Linux distributions

--- src/OS/Linux/Debian.hs
+++ src/OS/Linux/Debian.hs
@@ -33,29 +33,11 @@ debianAtom :: IO ()
 debianAtom = do
   installNix
 
-  putStrLn
-    "These packages will be added to nix configuration and installed:\n\
-    \  GHC                                                           \n\
-    \  cabal-install                                                 \n\
-    \  stack                                                         \n\
-    \  cabal2nix                                                     \n\
-    \  hoogle                                                        \n\
-    \  ghcid                                                         \n\
-    \  Atom                                                          \n\
-    \  HIE"
-
-  nixConfig <- (<> fromText ".config/nixpkgs/config.nix") <$> home
-  unlessM (testfile nixConfig) $ writeTextFile nixConfig "{\n  \n}"
-
-  addToLet
-    nixConfig
-    "all-hies = import (fetchTarball \"https://github.com/infinisil/all-hies/tarball/master\") {};"
-  addToLet nixConfig "unstable = import <nixpkgs> { inherit config; };"
-
-  importNixpkgs nixConfig
+  nixConfigFilePath <- (<> fromText ".config/nixpkgs/config.nix") <$> home
+  unlessM (testfile nixConfigFilePath) $ writeTextFile nixConfigFilePath "{\n  \n}"
 
   addToConfig
-    nixConfig
+    nixConfigFilePath
     "allowUnfree = true;                                                  \n\
     \                                                                     \n\
     \packageOverrides = pkgs: rec {                                       \n\
@@ -65,21 +47,18 @@ debianAtom = do
     \    paths = [                                                        \n\
     \      haskell.compiler.ghc865                                        \n\
     \      haskellPackages.cabal-install                                  \n\
-    \      unstable.haskellPackages.stack                                 \n\
-    \      unstable.haskellPackages.cabal2nix                             \n\
     \      haskellPackages.hoogle                                         \n\
-    \      haskellPackages.ghcid                                          \n\
     \      atom                                                           \n\
-    \      (all-hies.selection { selector = p: { inherit (p) ghc865; }; })\n\
+    \      ((import (fetchTarball \"https://github.com/infinisil/all-hies/tarball/master\") {}).selection { selector = p: { inherit (p) ghc865; }; })\n\
     \    ];                                                               \n\
     \  };                                                                 \n\
     \};"
-
+    
   shell (runAsUserPrefix "nix-env -i all") empty >>= \case
     ExitSuccess   -> putStrLn "Installation complete"
     ExitFailure n -> die $ "Installation failed with exit code: " <> repr n
 
-  let atomExtensions =
+  let atomPackages =
         [ "nix"
         , "atom-ide-ui"
         , "autocomplete-haskell"
@@ -92,11 +71,8 @@ debianAtom = do
         , "language-haskell"
         ]
 
-  putStrLn
-    $  "These Atom extensions will be installed:\n"
-    <> T.intercalate "\n" atomExtensions
-
-  mapM_ installAtomPackage atomExtensions
+  putStrLn $ T.unlines $ "These Atom extensions will be installed:" : atomPackages
+  mapM_ installAtomPackage atomPackages
 
 
 installNix :: IO ()
@@ -115,23 +91,6 @@ installNix =
           ExitSuccess -> putStrLn "nix successfully installed"
           ExitFailure n ->
             die $ "nix installation failed with exit code: " <> repr n
-
-importNixpkgs :: FilePath -> IO ()
-importNixpkgs path = do
-  config <- readTextFile path
-  unless ("with import <nixpkgs> {};" `T.isInfixOf` config)
-    $  writeTextFile path
-    $  "with import <nixpkgs> {};\n\n"
-    <> config
-
-addToLet :: FilePath -> T.Text -> IO ()
-addToLet path content = do
-  config <- readTextFile path
-  unless (content `T.isInfixOf` config)
-    $ writeTextFile path
-    $ if "let" `T.isInfixOf` config
-        then T.replace "\nin" ("\n  " <> content <> "\nin") config
-        else "let\n  " <> content <> "\nin\n" <> config
 
 addToConfig :: FilePath -> T.Text -> IO ()
 addToConfig path content = do
