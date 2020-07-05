3a6b2fafe0756f7dce020abed1c801a7ea24bd96
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-07-05 12:45:41 +0100

    55 - Refactor to use same Atom setup implementation on all Linux distributions

--- src/OS/Linux.hs
+++ src/OS/Linux.hs
@@ -27,60 +27,64 @@ data ExtensionInfo = ExtensionInfo MisoString Text
 nixOsAtom :: Sink Action -> IO ()
 nixOsAtom sink = do
   
-  mapM_ configureNixPackage $ uncurry ExtensionInfo <$>
+  mapM_ (configureNixPackage sink) $ uncurry ExtensionInfo <$>
     [ ("Haskell GHC", "haskell.compiler.ghc865")
     , ("cabal-install", "haskellPackages.cabal-install")
     , ("Atom", "atom")
     , ("Haskell IDE Engine", "((import (fetchTarball \"https://github.com/infinisil/all-hies/tarball/master\")\
     \ {}).selection { selector = p: { inherit (p) ghc865; }; })") ]
 
-  mapM_ configureAtomPackage [ "nix"
-                             , "atom-ide-ui"
-                             , "autocomplete-haskell"
-                             , "hasklig"
-                             , "ide-haskell-cabal"
-                             , "ide-haskell-hasktags"
-                             , "ide-haskell-hie"
-                             , "ide-haskell-hoogle"
-                             , "ide-haskell-repl"
-                             , "language-haskell" ]
+  mapM_ (configureAtomPackage sink) [ "nix"
+                                    , "atom-ide-ui"
+                                    , "autocomplete-haskell"
+                                    , "hasklig"
+                                    , "ide-haskell-cabal"
+                                    , "ide-haskell-hasktags"
+                                    , "ide-haskell-hie"
+                                    , "ide-haskell-hoogle"
+                                    , "ide-haskell-repl"
+                                    , "language-haskell" ]
     
-   where                     
-     appendLog text = sink $ Append (text <> "\n")
-     logStep text actions = do
-       appendLog $ "BEGIN : " <> text
-       _ <- actions
-       appendLog $ "END   : " <> text
-     configureNixPackage (ExtensionInfo name package) =
-       logStep ("Configuring " <> name) $ do
-         let configurationNixFile = "/etc/nixos/configuration.nix"
-         oldConfigurationNixText <- liftIO $ readFile configurationNixFile
+appendLog :: MisoString -> Sink Action -> IO ()
+appendLog text sink = sink $ Append (text <> "\n")
 
-         -- FIXME vvv requires Nix parsing using HNIX
-         let environmentSystemPackages = "environment.systemPackages = with pkgs; ["
-             newConfigurationNixText =
-               if isPackagePresent
-                 then oldConfigurationNixText
+logStep :: MisoString -> Sink Action -> IO a -> IO ()
+logStep text sink actions = do
+  appendLog ("BEGIN : " <> text) sink
+  _ <- actions
+  appendLog ("END   : " <> text) sink
+  
+configureNixPackage :: Sink Action -> ExtensionInfo -> IO ()
+configureNixPackage sink (ExtensionInfo name package) =
+  logStep ("Configuring " <> name) sink $ do
+    let configurationNixFile = "/etc/nixos/configuration.nix"
+    oldConfigurationNixText <- liftIO $ readFile configurationNixFile
 
-                 else replace
-                   environmentSystemPackages
-                   (environmentSystemPackages <> "\n\
-                        \    " <> package)
-                   oldConfigurationNixText
-               where isPackagePresent = package `isInfixOf` oldConfigurationNixText
-         -- ^^^
+    -- FIXME vvv requires Nix parsing using HNIX
+    let environmentSystemPackages = "environment.systemPackages = with pkgs; ["
+        isPackagePresent = package `isInfixOf` oldConfigurationNixText
+        newConfigurationNixText =
+          if isPackagePresent
+            then oldConfigurationNixText
+            else replace
+              environmentSystemPackages
+              (environmentSystemPackages <> "\n\
+                  \    " <> package)
+              oldConfigurationNixText
+    -- ^^^
 
-         liftIO $ writeFile configurationNixFile newConfigurationNixText
-         if oldConfigurationNixText == newConfigurationNixText -- OPTIMIZE
-           then appendLog "Nix package already installed"
-           else logStep (toMisoString package) (runShellCommand "nixos-rebuild switch")
+    liftIO $ writeFile configurationNixFile newConfigurationNixText
+    if oldConfigurationNixText == newConfigurationNixText -- OPTIMIZE
+      then appendLog "Nix package already installed" sink
+      else logStep (toMisoString package) sink (runShellCommand "nixos-rebuild switch")
 
-     -- TODO install or update? extension or log message
-     -- TODO ensure extensions are enabled if not enable them
-     configureAtomPackage package = do
-       --   check if package isAtomPackageInstalled
-       --   install if not installed
-       --   update if installed
-       let installingPackage = "Installing Atom package - " <> toMisoString package
-       logStep installingPackage $
-         installAtomPackage package
+-- TODO install or update? extension or log message
+-- TODO ensure extensions are enabled if not enable them
+configureAtomPackage :: Sink Action -> Text -> IO ()
+configureAtomPackage sink package = do
+  --   check if package isAtomPackageInstalled
+  --   install if not installed
+  --   update if installed
+  let installingPackage = "Installing Atom package - " <> toMisoString package
+  logStep installingPackage sink $
+    installAtomPackage package
